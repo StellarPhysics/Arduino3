9da603d6d99521277d3abffabc120e5129dc1daf
StellarPhysics <152861388+StellarPhysics@users.noreply.github.com>
2025-10-28 22:16:04 +0530

    Add files via upload

---
+++ all.cpp
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(8, INPUT);
+  pinMode(13, OUTPUT); 
+  pinMode(12, OUTPUT);
+}
+
+void loop() {  
+  if (digitalRead(2)) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+
+  if (!digitalRead(8)) {
+    digitalWrite(12, HIGH);
+  } else {
+    digitalWrite(12, LOW);
+  }
+}
